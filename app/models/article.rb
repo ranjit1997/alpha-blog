5153f6dfff26012ef429a13c6b0eaf148a2d3a67
ranjeet <rk1220245@gmail.com>
2020-08-18 15:23:09 +0530

    This is a creating action

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
-class Article < ApplicationRecord
+class ArticleRecord < ApplicationRecord
   # validate :title, presence: true, length: {minimum: 3, maximum:100}
   # validate :description, presence: true, length: {minimum: 10, maximum:300}
 end
