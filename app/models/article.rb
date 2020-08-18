e8f3976d5d26f24247b85d5a5f9286f978d7c8ba
ranjeet <rk1220245@gmail.com>
2020-08-18 13:05:02 +0530

    This is a get data to our webpage

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+  # validate :title, presence: true, length: {minimum: 3, maximum:100}
+  # validate :description, presence: true, length: {minimum: 10, maximum:300}
+end
