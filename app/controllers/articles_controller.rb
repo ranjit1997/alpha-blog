532b7ff1102b885ea3de62838094f0200dacc5e5
ranjeet <rk1220245@gmail.com>
2020-08-20 13:53:08 +0530

    Design the home page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,5 @@
 class ArticlesController < ApplicationController
-  def show
-    @article = Article.find(params[:id])
-  end
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
   def index
     @articles = Article.all
   end
@@ -9,11 +7,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def edit
-    @article = Article.find(params[:id])
+
   end
   def create
     # render plain: params[:article]
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     # render plain: @article.inspect
     if @article.save
       flash[:notice] = "Article was created successfully."
@@ -24,8 +22,8 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+
+    if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to @article
     else
@@ -33,12 +31,21 @@ class ArticlesController < ApplicationController
     end
   end
   def show
-    @article = Article.find(params[:id])
+
   end
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
     flash[:danger] = "Article was successfully deleted"
     redirect_to articles_path
+  end
+
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
   end
