a21e003e338654a289b9bfa4dc9db2e373a65f27
ranjeet <rk1220245@gmail.com>
2020-08-24 17:37:50 +0530

    some error

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_many :articles
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 25},
+            format: { with: VALID_EMAIL_REGEX }
+end
