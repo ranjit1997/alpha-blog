41b896cf275d3c17c884ed2e8516d7dadaf18c43
ranjeet <rk1220245@gmail.com>
2020-08-24 17:29:46 +0530

    some error

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_042617) do
+ActiveRecord::Schema.define(version: 2020_08_23_150103) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
